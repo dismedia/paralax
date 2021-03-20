c0f71e24d9dbb531493b1353b673e631df67db3b
CAP-MW <marcin.wykret@capsilon.com>
2021-03-20 17:24:01 +0100

    getting scroll values, not correct though

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import {domSource} from "./sources/sources";
+
 console.log("entry");
+
+
+domSource(window as any).subscribe((e)=>{
+    console.log(e)
+})
--- src/sources/sources.ts
+++ src/sources/sources.ts
@@ -1,6 +1,43 @@
-import {PositionValueSource} from "./domain";
-import {of} from "rxjs";
+import {PositionValue, PositionValueSource} from "./domain";
+import {combineLatest, from, fromEvent, merge, of} from "rxjs";
+import {map, startWith} from "rxjs/operators";
 
-export const simpleSource:PositionValueSource=of({fromPx:0,toPx:100});
+export const simpleSource: PositionValueSource = of({fromPx: 0, toPx: 100});
 
 
+function getScrollPercent() {
+    var docElement = document.documentElement,
+        body = document.body
+    return (docElement['scrollTop'] || body['scrollTop']) / ((docElement['scrollHeight'] || body['scrollHeight']) - docElement.clientHeight) * 100;
+}
+
+
+const extractPositionValues = (window) => {
+
+    const docElement = document.documentElement;
+    const body = window.document.body
+
+    if(!body) return null;
+
+    //not correnc yet
+
+    return {
+
+
+        fromPx: window.pageYOffset,
+        toPx: window.pageYOffset + window.outerHeight,
+
+        p:window.pageYOffset/(window.document.documentElement.scrollHeight-window.outerHeight)
+
+    } as PositionValue
+
+
+}
+
+export const domSource = (window) => merge(
+    fromEvent(window, "resize"),
+    fromEvent(window, "scroll")
+).pipe(
+    map((resizeEvent, ScrollEvent) => extractPositionValues(window)),
+    startWith(extractPositionValues(window))
+)
