d3eccef7dacd6cc19800a362ba4b68ee5cc42488
CAP-MW <marcin.wykret@capsilon.com>
2021-03-23 16:31:08 +0100

    clean

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,3 @@ import {domSource} from "./sources/sources";
 console.log("entry");
 
 
-setTimeout(()=>{
-
-    domSource(window as any)
-        .subscribe((e)=>{
-            console.log(e)
-        })
-
-
-    const el=(window as any).document.body.querySelector("#el1")
-
-
-    domSource(window as any)
-        .pipe(
-            affectPosition(el)
-        )
-        .subscribe((e)=>{})
-
-
-
-
-},100)
-
